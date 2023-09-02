d564a3058d35a4d9e79d988ce8738c8f6a1f00f7
wujiang_person@163.com <wujiang_person@163.com>
2023-09-02 14:57:42 +0800

    add 集成 camunda工作流脚本
    Signed-off-by: wujiang_person@163.com <wujiang_person@163.com>

--- src/main/java/com/bigtree/aitest/AiTestApplication.java
+++ src/main/java/com/bigtree/aitest/AiTestApplication.java
@@ -1,11 +1,13 @@
 package com.bigtree.aitest;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
 @EnableWebSocket
+@EnableProcessApplication
 public class AiTestApplication {
 
 	public static void main(String[] args) {
